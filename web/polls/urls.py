2f1e96c1c637e281697d2bd3fa028a6b8dfe113e
Da Yeong <yoon351200@naver.com>
2023-05-04 01:11:18 +0900

    html 연결

--- web/polls/urls.py
+++ web/polls/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index'),
+    path("", views.index, name="index"),
+    path("analysis/", views.analysis, name="analysis"),
+    path("login/", views.login, name="login"),
+    path("mypage/", views.mypage, name="mypage"),
+    path("recording/", views.recording, name="recording"),
+    path("signUp/", views.signUp, name="signUp"),
 ]
--- web/polls/views.py
+++ web/polls/views.py
@@ -8,17 +8,27 @@ def index(request):
     template = loader.get_template("polls/index.html")
     return render(request, "polls/index.html")
 
-# def analysis(request):
-#     return HttpResponse("analysis")
 
-# def login(request):
-#     return HttpResponse("login")
+def analysis(request):
+    template = loader.get_template("polls/analysis.html")
+    return render(request, "polls/analysis.html")
 
-# def mypage(request):
-#     return HttpResponse("mypage")
 
-# def signUp(request):
-#     return HttpResponse("signup")
+def login(request):
+    template = loader.get_template("polls/login.html")
+    return render(request, "polls/login.html")
 
-# def recording(request):
-#     return HttpResponse("recording")
+
+def mypage(request):
+    template = loader.get_template("polls/mypage.html")
+    return render(request, "polls/mypage.html")
+
+
+def signUp(request):
+    template = loader.get_template("polls/signUp.html")
+    return render(request, "polls/signUP.html")
+
+
+def recording(request):
+    template = loader.get_template("polls/recording.html")
+    return render(request, "polls/recording.html")
